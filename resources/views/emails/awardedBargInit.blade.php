5cfd175a3e6363184558f1bbd7557e8af6ff203d
Mualter8 <muugotwings@gmail.com>
2021-01-05 01:38:47 +0300

    Remove all

--- resources/views/emails/awardedBargInit.blade.php
+++
@@ -1,5 +0,0 @@
-Hey {{ $user->username }},
-
-Great job, {{ $blueprint->actor->username }} just set your post as the best answer in the discussion '{{ $blueprint->discussion->title }}'. Thanks for helping out.
-
-View it here: {{ app()->url() }}/d/{{ $blueprint->discussion->id }}-{{ $blueprint->discussion->slug }}
--- resources/views/emails/bargInitSetInDiscussion.blade.php
+++
@@ -1,5 +0,0 @@
-Hey {{ $user->username }},
-
-{{ $blueprint->actor->username }} just set a best answer in the discussion '{{ $blueprint->discussion->title }}', which you participated in.
-
-Check it out: {{ app()->url() }}/d/{{ $blueprint->discussion->id }}-{{ $blueprint->discussion->slug }}
