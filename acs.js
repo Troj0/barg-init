8c13acf6bec5874e4847d5b9991f763f04a34580
Al <mu8@tuta.io>
2022-02-24 17:26:15 +0300

    Update acs.js

--- acs.js
+++ acs.js
@@ -225,3 +225,38 @@
             });
         });
     </script>-->
+<!--<script type="text/javascript" charset="utf-8">
+        function setCookie(name, value, days) {
+            if (days) {
+                var date = new Date();
+                date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+                var expires = "; expires=" + date.toGMTString();
+            } else
+                var expires = "";
+            document.cookie = name + "=" + value + expires;
+        }
+
+        function getCookie(name) {
+            var nameEQ = name + "=";
+            var ca = document.cookie.split(';');
+            for (var i = 0; i < ca.length; i++) {
+                var c = ca[i];
+                while (c.charAt(0) == ' ')
+                    c = c.substring(1, c.length);
+                if (c.indexOf(nameEQ) == 0)
+                    return c.substring(nameEQ.length, c.length);
+            }
+            return null;
+        }
+
+        var cerezDeger = getCookie("cerez");
+        if (cerezDeger == null) {
+            $(".cerezNotification").show(500);
+            $("#cerezNotificationHide").click(function () {
+                $(".cerezNotification").hide(500);
+                setCookie("cerez", "deger", 1);
+            });
+
+        }
+        loadingFunction()
+    </script>-->
